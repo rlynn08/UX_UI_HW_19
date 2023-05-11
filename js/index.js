7148dc2553e6c9f4b0f65922a5ce6f194e14511e
Robin Bolton <rlynn08@gmail.com>
2023-05-11 06:41:21 -0400

    adding event listener for modal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 console.log("your_index.js file is loaded correctly!");
+// $(".contactImage") .on("click", (function() {
+
+// });
+
+
+$(document).ready(function(){
+$("#closeModal").click(function(){
+    $(".contactImage").hide();
+    console.log("hidden");
+  })
+  
+  $("#contactLink").click(function(){
+    $(".contactImage").show();
+    console.log("display");
+  });
+});
